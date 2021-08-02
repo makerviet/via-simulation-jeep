cc6c5fad104beedef3b8744f76cb5b99e5b4b851
StupidWizard <toinguyenkhac@gmail.com>
2021-08-02 18:43:49 +0700

    Update CommandServer and control car

---
+++ Assets/1_SelfDrivingCar/Scripts/CarRemoteControl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof(CarController))]
+    public class CarRemoteControl : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+
+        public Camera FrontFacingCamera;
+
+        public float SteeringAngle { get; set; }
+        public float Acceleration { get; set; }
+        private Steering s;
+
+        private float _throttle = 0;
+        private float _steering = 0;
+        private bool _controlUpdated = false;
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+            s = new Steering();
+            s.Start();
+        }
+
+        private void Start()
+        {
+            CommandServer.RegisterSimulator(this, FrontFacingCamera);
+        }
+
+        public void UpdateSteering(float steering, float throttle)
+        {
+            print("Throttle: " + throttle + " Steering: " + steering);
+            this._throttle = throttle;
+            this._steering = steering;
+            this._controlUpdated = true;
+        }
+
+        void UpdateCarControl()
+        {
+            if (_controlUpdated)
+            {
+                _controlUpdated = false;
+                SteeringAngle = _steering;
+
+                if (m_Car.CurrentSpeed / m_Car.MaxSpeed < _throttle)
+                {
+                    Acceleration = 0.5f;
+                }
+                else
+                {
+                    Acceleration = 0.0f;
+                }
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            UpdateCarControl();
+
+            // If holding down W or S control the car manually
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+            {
+                s.UpdateValues();
+                m_Car.Move(s.H, s.V, s.V, 0f);
+            } else
+            {
+				m_Car.Move(SteeringAngle, Acceleration, Acceleration, 0f);
+            }
+        }
+
+        public float CurrentSteerAngle => m_Car.CurrentSteerAngle;
+        public float AccelInput => m_Car.AccelInput;
+        public float CurrentSpeed => m_Car.CurrentSpeed;
+    }
+}
--- Assets/1_SelfDrivingCar/Scripts/CommandServer.cs
+++ Assets/1_SelfDrivingCar/Scripts/CommandServer.cs
@@ -9,19 +9,38 @@ using System.Security.AccessControl;
 
 public class CommandServer : MonoBehaviour
 {
+	private static CommandServer Instance;
+
+    #region socket
+    private static WebSocketServer _webSocket;
+    #endregion
+
+    #region control on simulator
+	[Header("Debug")]
+    public Camera FrontFacingCamera;
 	public CarRemoteControl CarRemoteControl;
-	public Camera FrontFacingCamera;
-	private CarController _carController;
-	private static WebSocketServer _webSocket;
+	//private CarController _carController;
+    #endregion
 
-	private float _throttle = 0;
-	private float _steering = 0;
-	private bool _controlUpdated = false;
+    
 
-	// Use this for initialization
-	void Start()
+    private void Awake()
+    {
+		if (Instance == null)
+		{
+			Instance = this;
+			GameObject.DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			GameObject.DestroyImmediate(this.gameObject);
+		}
+    }
+
+    // Use this for initialization
+    void Start()
 	{
-		_carController = CarRemoteControl.GetComponent<CarController>();
+		//_carController = CarRemoteControl.GetComponent<CarController>();
 		if (_webSocket == null) {
 			_webSocket = new WebSocketServer (4567);
 			_webSocket.AddWebSocketService<SocketService>("/simulation", () => new SocketService(this.OnSteer) { IgnoreExtensions = true });
@@ -29,56 +48,75 @@ public class CommandServer : MonoBehaviour
 		}
 	}
 
+	public static void RegisterSimulator(CarRemoteControl pCarRemoteController, Camera pFrontFacingCamera)
+	{
+		Instance.DoRegisterSimulator(pCarRemoteController, pFrontFacingCamera);
+	}
+
+	public static void OnSimulatorDestroyed(CarRemoteControl pCarRemoteController)
+	{
+		Instance.DoRemoveSimulator(pCarRemoteController);
+	}
+
+	void DoRemoveSimulator(CarRemoteControl pCarRemoteController)
+	{
+		if (this.CarRemoteControl == pCarRemoteController)
+		{
+			this.CarRemoteControl = null;
+			FrontFacingCamera = null;
+		}
+	}
+
+	void DoRegisterSimulator(CarRemoteControl pCarRemoteController, Camera pFrontFacingCamera)
+	{
+		this.CarRemoteControl = pCarRemoteController;
+		this.FrontFacingCamera = pFrontFacingCamera;
+	}
+
 	// Update is called once per frame
 	void Update()
 	{
 		EmitTelemetry();
-		UpdateCarControl();
-	}
-
-	void UpdateCarControl() {
-		if (_controlUpdated) {
-			_controlUpdated = false;
-			CarRemoteControl.SteeringAngle = _steering;
-			if (_carController.CurrentSpeed / _carController.MaxSpeed < _throttle) {
-				CarRemoteControl.Acceleration = 0.5f;
-			} else {
-				CarRemoteControl.Acceleration = 0.0f;
-			}
-		}
 	}
 
 	void OnSteer(float throttle, float steering)
 	{
-		print("Throttle: " + throttle + " Steering: " + steering);
-		this._throttle = throttle;
-		this._steering = steering;
-		this._controlUpdated = true;
+		if (IsOnSimulating)
+		{
+			CarRemoteControl.UpdateSteering(steering: steering, throttle: throttle);
+		}
 	}
 
 	void EmitTelemetry()
 	{
+		if (!IsOnSimulating)
+		{
+			return;
+		}
+
 		UnityMainThreadDispatcher.Instance().Enqueue(() =>
 		{
 			if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S))) {
 				// Manual mode
 				Dictionary<string, string> data = new Dictionary<string, string>();
-				data["steering_angle"] = _carController.CurrentSteerAngle.ToString("N4");
-				data["throttle"] = _carController.AccelInput.ToString("N4");
-				data["speed"] = _carController.CurrentSpeed.ToString("N4");
+				data["steering_angle"] = CarRemoteControl.CurrentSteerAngle.ToString("N4");
+				data["throttle"] = CarRemoteControl.AccelInput.ToString("N4");
+				data["speed"] = CarRemoteControl.CurrentSpeed.ToString("N4");
 				data["image"] = Convert.ToBase64String(CameraHelper.CaptureFrame(FrontFacingCamera));
 				_webSocket.WebSocketServices["/simulation"].Sessions.Broadcast(JsonHandler.FromDictionaryToJson(data));
 			}
 			else {
 				// Autonomous mode
 				Dictionary<string, string> data = new Dictionary<string, string>();
-				data["steering_angle"] = _carController.CurrentSteerAngle.ToString("N4");
-				data["throttle"] = _carController.AccelInput.ToString("N4");
-				data["speed"] = _carController.CurrentSpeed.ToString("N4");
+				data["steering_angle"] = CarRemoteControl.CurrentSteerAngle.ToString("N4");
+				data["throttle"] = CarRemoteControl.AccelInput.ToString("N4");
+				data["speed"] = CarRemoteControl.CurrentSpeed.ToString("N4");
 				data["image"] = Convert.ToBase64String(CameraHelper.CaptureFrame(FrontFacingCamera));
 				_webSocket.WebSocketServices["/simulation"].Sessions.Broadcast(JsonHandler.FromDictionaryToJson(data));
 			}
 		});
 	}
 
+
+	bool IsOnSimulating => (CarRemoteControl != null && FrontFacingCamera != null);
 }
