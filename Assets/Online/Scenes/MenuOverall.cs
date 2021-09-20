5b08a7cb0dc6fe10e837bd995c9b810eca7be3e3
StupidWizard <toinguyenkhac@gmail.com>
2021-09-20 08:29:39 +0700

    Select mode Play online from Menu

---
+++ Assets/Online/Scenes/MenuOverall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOverall : MonoBehaviour
+{
+
+    public void StartHost()
+    {
+        SceneManager.LoadScene("MenuOnline_Host");
+    }
+
+    public void StartClient()
+    {
+        SceneManager.LoadScene("MenuOnline_Client");
+    }
+}
