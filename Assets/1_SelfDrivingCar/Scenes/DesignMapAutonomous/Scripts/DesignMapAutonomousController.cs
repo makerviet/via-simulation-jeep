3b2f872878eae7cc7e3e623688d51b48ee5b4287
StupidWizard <toinguyenkhac@gmail.com>
2021-11-28 16:13:36 +0700

    countdown 3s to start Control Car; update speed UI

---
+++ Assets/1_SelfDrivingCar/Scenes/DesignMapAutonomous/Scripts/DesignMapAutonomousController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesignMapAutonomousController : MonoBehaviour
+{
+    [SerializeField] CarRemoteControl mCar;
+
+    private void Start()
+    {
+        StartCoroutine(DoCountDownToStart());
+    }
+
+    IEnumerator DoCountDownToStart()
+    {
+        for (int i = 3; i >= 1; i--)
+        {
+            // 
+            yield return new WaitForSeconds(1);
+        }
+
+        mCar.StartControl();
+    }
+}
